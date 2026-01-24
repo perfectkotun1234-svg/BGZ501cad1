c362b7aea968d365f922d2c83760273810d132d4
perfectkotun1234-svg <perfectkotun1234@gmail.com>
2026-01-24 08:42:30 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,4 +2,4 @@
     v0.01
 ]]
 
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/prosecutioned/ancient/main/initialize.lua"))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/perfectkotun1234-svg/BGZ501cad1/refs/heads/main/initialize.lua"))()
